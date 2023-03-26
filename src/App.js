749854852eb933423c7c985164c3852ce11f9a93
Arthur Dias <arthurdvm2@gmail.com>
2023-03-26 14:56:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    municipios: [],
+  };
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/municipios/MG/')
+      .then(response => {
+        console.log(response.data); // Debug do resultado da API
+        this.setState({ municipios: response.data });
+      })
+      .catch(error => console.log(error)); // Debug do erro, se ocorrer
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Municípios de MG</h1>
+        <ul>
+          {this.state.municipios.map(municipio => (
+            <li key={municipio.ibge_code}>{municipio.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
